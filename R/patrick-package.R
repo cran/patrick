b451a279bc577ce255573b5e047137113c0a42ed
Michael Quinn <msquinn@google.com>
2018-08-13 13:10:03 +0000

    version 0.0.1

---
+++ R/patrick-package.R
@@ -0,0 +1,29 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Parameterized Unit Testing
+#'
+#' `patrick` (parameterized testing in R is kind of cool!) is a `testthat`
+#' extension that lets you create reusable blocks of a test codes. Parameterized
+#' tests are often easier to read and more reliable, since they follow the DNRY
+#' (do not repeat yourself) rule.
+#'
+#' This package is inspired by parameterized testing packages in other
+#' languages, notably the
+#' [`parameterized`](https://github.com/wolever/parameterized) library in
+#' Python.
+#'
+#' @name patrick
+#' @docType package
+NULL
